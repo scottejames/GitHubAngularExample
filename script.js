46b0ac508b53ef9a295af51f261c80f46f71af72
scott james <scottejames@gmail.com>
2017-02-06 11:42:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+(function() {
+
+    var createWorker = function() {
+
+        var workCount = 0;
+
+        var task1 = function() {
+            workCount += 1;
+            console.log("task1 " + workCount);
+        };
+
+        var task2 = function() {
+            workCount += 1;
+            console.log("task2 " + workCount);
+        };
+
+
+        return {
+            job1: task1,
+            job2: task2
+        };
+
+    };
+
+
+    var worker = createWorker();
+
+
+    worker.job1();
+    worker.job2();
+    worker.job2();
+    worker.job2();
+}());/**
+ * Created by scottejames on 06/02/2017.
+ */
